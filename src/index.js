87b56da5b09d916e94f2641f2d22a17d696af977
L9628 <mhp4718@gmail.com>
2022-08-17 00:24:56 +0900

    Recap Refactor

--- src/index.js
+++ src/index.js
@@ -5,18 +5,29 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
 // reducer는 data를 바꾸고 modify 하는 function 여기선 countModifier
 // state는 변경이 필요한 data 여기선 count
 // action는 redux에서 function을 부를 때 쓰는 두번째 parameter으로 reducer와 소통하기 위한 방법
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
 };
 
 // store는 data를 담는 저장소 여기선 countStore
@@ -30,11 +41,11 @@ countStore.subscribe(onChange);
 
 // reduer에게 action을 보내는 방법 store.dispatch({key: value})
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
