b0f10ddf181a0f4aa869217f9f822074bfe28735
L9628 <mhp4718@gmail.com>
2022-08-17 00:10:00 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ const number = document.querySelector("span");
 
 // reducer는 data를 바꾸고 modify 하는 function 여기선 countModifier
 // state는 변경이 필요한 data 여기선 count
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 // store는 data를 담는 저장소 여기선 countStore
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
 
 // let count = 0;
