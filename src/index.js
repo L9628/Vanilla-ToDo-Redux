08382f68a8d714156e24f43fa5e1abdb927988f2
L9628 <mhp4718@gmail.com>
2022-08-17 00:16:52 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
 // reducer는 data를 바꾸고 modify 하는 function 여기선 countModifier
 // state는 변경이 필요한 data 여기선 count
+// action는 redux에서 function을 부를 때 쓰는 두번째 parameter으로 reducer와 소통하기 위한 방법
 const countModifier = (count = 0, action) => {
   console.log(count, action);
   if (action.type === "ADD") {
@@ -20,10 +22,23 @@ const countModifier = (count = 0, action) => {
 // store는 data를 담는 저장소 여기선 countStore
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+// reduer에게 action을 보내는 방법 store.dispatch({key: value})
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 console.log(countStore.getState());
 
