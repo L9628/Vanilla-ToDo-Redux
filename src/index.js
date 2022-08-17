298591d79e06a26c5bd825aa22aa55d3a2d8afe3
L9628 <mhp4718@gmail.com>
2022-08-17 19:09:46 +0900

    Setup

--- src/index.js
+++ src/index.js
@@ -1,75 +1,18 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-// reducer는 data를 바꾸고 modify 하는 function 여기선 countModifier
-// state는 변경이 필요한 data 여기선 count
-// action는 redux에서 function을 부를 때 쓰는 두번째 parameter으로 reducer와 소통하기 위한 방법
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-  // if (action.type === "ADD") {
-  //   return count + 1;
-  // } else if (action.type === "MINUS") {
-  //   return count - 1;
-  // } else {
-  //   return count;
-  // }
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-// store는 data를 담는 저장소 여기선 countStore
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
 };
 
-countStore.subscribe(onChange);
-
-// reduer에게 action을 보내는 방법 store.dispatch({key: value})
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
-console.log(countStore.getState());
-
-// let count = 0;
-
-// number.innerText = count;
-
-// const updateText = () => {
-//   number.innerText = count;
-// };
-
-// const handleAdd = () => {
-//   count++;
-//   updateText();
-// };
-
-// const handleMinus = () => {
-//   count--;
-//   updateText();
-// };
-
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
